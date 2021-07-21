917223f2d316b74da1144dfe6a752a7d89c5d0c3
Vasilisa <Vasilisandreeva00@gmail.com>
2021-07-21 18:51:11 +0300

    fff

--- classwork/classwork2.js
+++ classwork/classwork2.js
@@ -101,20 +101,25 @@
 
 
 // - Створити цикл for на 100 ітерацій з кроком 1. Вивести поточний номер кроку через console.log та document.write
-for (let i = 0; i <100; i++){
-        console.log(i)
-        document.write(i)
-}
-
+// for (let i = 0; i <100; i++){
+//         console.log(i)
+//         document.write(i)
+// }
 
 // - Створити цикл for на 100 ітерацій з кроком 2. Вивести поточний номер кроку через console.log та document.write
 
-// - Створити цикл for на 100 ітерацій. Вивести тільки парні кроки. через console.log + document.write
+// // - Створити цикл for на 100 ітерацій. Вивести тільки парні кроки. через console.log + document.write
 // for (let i = 0; i <100; i++){
-//     if (i % 2 == 0){
+//     if (i % 2 === 0){
 //         console.log(i)
 //         document.write(i)
 //     }
 // }
 
-// - Створити цикл for на 100 ітерацій. Вивести тільки непарні кроки. через console.log + document.write
+// // - Створити цикл for на 100 ітерацій. Вивести тільки непарні кроки. через console.log + document.write
+// for (let i = 0; i <100; i++){
+//     if (i % 2 !== 0){
+//         console.log(i)
+//         document.write(i)
+//     }
+// }
