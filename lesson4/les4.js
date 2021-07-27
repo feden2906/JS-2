ebb5ec2db182f21b7319cd35a936ec041941ddf5
Vasilisa <Vasilisandreeva00@gmail.com>
2021-07-27 21:02:44 +0300

    fff

--- lesson4/les4.js
+++ lesson4/les4.js
@@ -32,8 +32,19 @@
 // console.log(true)
 
 
-// - створити масив рандомних цілих числових значень (або згенерувати, за допомоги попередньої функції) . за допомоги map та колбеку перетворити всі об'єкти в масиві на срінгові.
-let mass = [11, 222, 345, 434, 3, 43, 436, 76, 58, 56, 54, 97, 36, 785]
+// // // - створити масив рандомних цілих числових значень (або згенерувати, за допомоги попередньої функції) . за допомоги map та колбеку перетворити всі об'єкти в масиві на срінгові.
+// let mass = [11, 222, 345, 434, 3, 43, 436, 76, 58, 56, 54, 97, 36, 785]
+// let map = mass.map(function (value)
+// {
+//     return{
+//         name: value
+//     }
+// })
+//
+// function getItem(callBack, number){
+//     console.log('Покажи мені', number);
+// }
+// getItem('fgbvla',11)
 
 
 // // - Створити функцію конструктор для об'єктів User з полями id, name, surname , email, phone
@@ -60,4 +71,36 @@ let mass = [11, 222, 345, 434, 3, 43, 436, 76, 58, 56, 54, 97, 36, 785]
 // user.push(aa, ab, ac, ad, ae, af, ag, ah, ai, aj);
 // console.log(user);
 
-// Взяти масив цей  User[] та: Відфільтрувати , залишивши тільки об'єкти з парними id (filter) та Відсортувати його по id. по зростанню (sort)
+// // Взяти масив цей  User[] та: Відфільтрувати , залишивши тільки об'єкти з парними id (filter) та Відсортувати його по id. по зростанню (sort)
+// function User(id, name, surname, email, phone){
+//     this.id = id;
+//     this.name = name;
+//     this.surname = surname;
+//     this.email = email;
+//     this.phone = phone;
+// }
+// let user = []
+// const aa = new User(23424, 'aa', 'ваива', 'avbivbib@gmail.com', 45346);
+// const ab = new User(24523423, 'ab', 'апріа', 'avbivbib@gmail.com', 245624);
+// const ac = new User(85485, 'ac', 'іар', 'avbivbib@gmail.com', 2464565);
+// const ad = new User(345345, 'ad', 'іапі', 'avbivbib@gmail.com', 45645);
+// const ae = new User(345345, 'ae', 'іра', 'avbivbib@gmail.com', 34652465);
+// const af = new User(345345, 'af', 'солвенп', 'avbivbib@gmail.com', 45624);
+// const ag = new User(345354, 'ag', 'чаок', 'avbivbib@gmail.com', 24562);
+// const ah = new User(2433, 'ah', 'кіркі', 'avbivbib@gmail.com', 25626);
+// const ai = new User(2543, 'ai', 'мкер', 'avbivbib@gmail.com', 25462);
+// const aj = new User(8843, 'aj', 'мркер', 'avbivbib@gmail.com', 256256);
+//
+// user.push(aa, ab, ac, ad, ae, af, ag, ah, ai, aj);
+// console.log(user);
+//
+// let idF = user.id.filter(function (id){
+//     return (user.id % 2) !== 0
+// })
+// console.log(idF)
+//
+// user.id.sort(function(a, b){
+//     console.log(a, 'a')
+//     console.log(b, 'b')
+//     return a - b
+// })
