82737a01e8f70342f49ae0df97d4b597b3879230
Vasilisa <Vasilisandreeva00@gmail.com>
2021-07-27 23:34:44 +0300

    fff

---
+++ lesson5/les5.js
@@ -0,0 +1,26 @@
+// Візьміть файл template1.html, підключіть до нього скрипт, і працюйте в ньому!
+//
+//     - Напишіть код,  який за допомоги document.getElementById або document.getElementsByClassName або document.getElementsByTagName :
+
+// // -- отримує текст з параграфа з id "content"
+// let elementById = document.getElementById('content')
+// console.log(elementById);
+
+// // -- отримує текст з блоку з id "rules"
+// let elementById = document.getElementById('rules')
+// console.log(elementById);
+
+// -- замініть текст параграфа з id 'content' на будь-який інший
+document.getElementById('content')
+
+// -- замініть текст параграфа з id 'rules' на будь-який інший
+
+// -- змініть кожному елементу колір фону на червоний
+
+// -- змініть кожному елементу колір тексту на синій
+
+// -- отримати весь список класів елемента з id=rules і вивести їх в console.log
+
+// -- отримати всі елементи з класом fc_rules. визначити їм 2 події "клік", одна вивдоить текст елементу, інша довільний ьексь
+
+// -- поміняти колір тексту у всіх елементів fc_rules на червоний
