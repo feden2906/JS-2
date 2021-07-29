0ad49361f9952d4becef687bd0f860eb12a8ba95
Vasilisa <Vasilisandreeva00@gmail.com>
2021-07-29 19:56:06 +0300

    fff

--- classwork/classwork5.js
+++ classwork/classwork5.js
@@ -30,17 +30,36 @@
 //     li.style.backgroundColor = 'silver';
 // }
 
-// f) отримує всі елементи 'a' та додає їм клас anchor
-let elementsByTagNameA = document.getElementsByTagName('a');
-elementsByTagNameA.classList.add('anchor');
-console.log(elementsByTagNameA);
+// // f) отримує всі елементи 'a' та додає їм клас anchor
+// let elementsByTagNameA = document.getElementsByTagName('a');
+// for (const item of elementsByTagNameA) {
+//     item.classList.add('anchor');
+//     console.log(elementsByTagNameA);
+// }
 
-// g) отримує всі елементи 'a' та у випадку, якщо текстовий контен елементу дорівнює link3, змінює йому розмір тексту на 40 пікселів
-
+// // g) отримує всі елементи 'a' та у випадку, якщо текстовий контент елементу дорівнює link3, змінює йому розмір тексту на 40 пікселів
+// let elementsByTagName = document.getElementsByTagName('a');
+// for (const itemA of elementsByTagName) {
+//     if (itemA.innerText === 'link3'){
+// itemA.style.fontSize = '40px';
+// console.log(itemA);
+//     }
+//     console.log(itemA.innerText);
+// }
 
 // h) отримує всі елементи 'a' та додає їм клас element_XXX. Де XXX - текстовий контент елементу a
+let elementsByTagNameA= document.getElementsByTagName('a');
+for (const elementA of elementsByTagNameA) {
+    elementA.classList.add('element_');
+    console.log(elementA);
+}
+
 
 // i) отримує всі елементи 'sub-header' та змінює колір фону. Фон отримати з prompt()
+let classNameSubHeader = document.getElementsByClassName('sub-header');
+for (const classNameSubHeaderElement of classNameSubHeader) {
+    classNameSubHeaderElement.style.backgroundColor = prompt()
+}
 
 // j) отримує всі елементи 'sub-header' та змінює колір тексту у видаку якщо текст елемнту = content 2 segment . Колір отримати з prompt()
 
