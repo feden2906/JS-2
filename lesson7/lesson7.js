a7b9ae52ebff20b6c6cb1db489b5cb16ce73cf6d
Vasilisa <Vasilisandreeva00@gmail.com>
2021-08-04 15:44:19 +0300

    fff

--- lesson7/lesson7.js
+++ lesson7/lesson7.js
@@ -8,189 +8,206 @@
 // При тому ваші дії мають бути синхронізовані.
 
 //1
-function wakeUp(time, cb) {
-    setTimeout(() => {
-        console.log('потрібно прокинутись');
-        if (time < '7-00') {
-            console.log('спи', null);
-            cb('ще рано');
-        } else {
-            console.log('прокидайся')
-            cb(null, 'зараз');
-        }
-    }, 300)
+function wakeUp(time) {
+    return new Promise( (resolve, reject) => {
+        setTimeout(() => {
+            console.log('потрібно прокинутись');
+            if (time < '7-00') {
+                console.log('спи');
+                reject ('ще 5 хв');
+            } else {
+                console.log('прокидайся')
+                resolve ('час вставати');
+            }
+        }, 300)
+    })
 }
 function getUp(){
-    setTimeout(() =>{
-        console.log('вставай з ліжка')
-    }, 111)
+    return new Promise((resolve, reject) => {
+        setTimeout(() =>{
+            console.log('вставай з ліжка')
+            resolve('застели постіль')
+        }, 111)
+    })
 }
-wakeUp('7-00', (err, wakeUp) => {
-    if(err){
-        console.error(err)
-    } else{
-        getUp();
-        console.log(wakeUp)
-    }
-});
-
-
-
-//2
-function washFace (time, cb){
-    setTimeout(() => {
-        console.log('вмивайся');
-        if (time < '7-15'){
-            console.log('іди в ванну', null)
-            cb('повзи давай')
-        } else {
-            console.log('чисть зуби')
-            cb(null, 'воду закрий')
-        }
-    }, 100)
+function washFace (time) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            console.log('вмивайся');
+            resolve('воду закрий')
+        }, 100)
+    })
 }
 function getDressed() {
-    setTimeout(() => {
-        console.log('одягайся')
-    }, 116)
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            console.log('одягайся')
+            resolve('швидко')
+        }, 116)
+    })
 }
-
-washFace('7-25', (err, washFace) => {
-    if(err){
-        console.error(err)
-    } else {
-getDressed();
-console.log(washFace)
-    }
-})
-
-
-//3
-function breakfast (money, cb){
-    setTimeout(()=>{
-        console.log('Ваше замовлення')
-        if(money < 50){
-            console.log('яєшня', null)
-            cb('вам вистачить лише на')
-        } else{
+function breakfast (money){
+    return new Promise((resolve, result) => {
+        setTimeout(()=>{
+            console.log('Ваше замовлення')
             console.log('англійський сніданок')
-            cb (null, 'смачного')
-        }
+            resolve('смачного')
         },1000);
+    })
+}
+function pay(money){
+    return new Promise((resolve, reject) =>{
+      setTimeout(() =>{
+          console.log('ваш рахунок')
+          resolve('оплатити рахунок')
+      }, 540)
+    })
+}
+function manicure(money){
+    return new Promise((resolve, reject) => {
+        setTimeout(()=> {
+            console.log('Зробити манікюр')
+            resolve('ваш майстер Аліна')
+        }, 1400);
+    })
+}
+function goToWork (money){
+    return new Promise((resolve, reject) => {
+        setTimeout(()=>{
+            console.log('Добратись на роботу')
+            resolve('біжи на автобус')
+        }, 3000);
+    })
+}
+function coffe(money){
+    return new Promise((resolve, reject) => {
+        setTimeout(()=>{
+            console.log('купити каву')
+            resolve('американо')
+        }, 700);
+    })
+}
+function talkToFriend(time){
+    return new Promise(((resolve, reject) => {
+        setTimeout(()=>{
+            console.log('Поговорити з подругою')
+            resolve('розказати новини')
+        }, 1500);
+    }))
+}
+function chat(time){
+    return new Promise(((resolve, reject) => {
+        setTimeout(()=>{
+            console.log('відписатись в чатіку')
+            resolve('мемасікі вогонь')
+        }, 360);
+    }))
+}
+ function goToGym(time){
+    return new Promise(((resolve, reject) => {
+        setTimeout(()=>{
+        console.log('Сходити на треню')
+            resolve('відпрацювати кроваві техніки')
+    }, 2200);
+    }))
+ }
+ function readTheBook(time){
+    return new Promise(((resolve, reject) => {
+        setTimeout(()=>{
+            console.log('Читати книгу')
+            resolve('записати круті думки')
+        }, 860);
+    }))
+ }
+function bujet(money){
+    return new Promise(((resolve, reject) => {
+        setTimeout(()=>{
+            console.log('Розпланувати бюджет')
+            resolve('запланувати покупку')
+        }, 1150);
+    }))
+}
+function byDress(money){
+    return new Promise(((resolve, reject) => {
+        setTimeout(()=>{
+            console.log('Обрати плаття')
+            resolve('купити найкрасивіше')
+        }, 890);
+    }))
+}
+function lanch(money){
+    return new Promise(((resolve, reject) => {
+        setTimeout(()=>{
+            console.log('Пообідати')
+            resolve('комплексний обід')
+        }, 1250);
+    }))
 }
 
-function enjoy(){
-    setTimeout(() => {
-        console.log('смачного')
-    }, 230)
-}
-breakfast(100, (err, breakfast) => {
-    if (err){
-        console.log(err)
-    } else {
-        enjoy()
-        console.log(breakfast)
-    }
-})
+wakeUp('7-00')
+    .then((value) =>{
+        console.log(value);
+        return getUp();
+    })
+    .then((value) =>{
+        console.log(value);
+        return washFace();
+    })
+    .then((value) =>{
+        console.log(value);
+        return getDressed();
+        })
+    .then((value) => {
+        console.log(value);
+        return breakfast();
+    })
+    .then((value) => {
+        console.log(value);
+        return pay();
+    })
+    .then((value) => {
+        console.log(value);
+        return manicure();
+    })
+    .then((value) => {
+        console.log(value);
+        return goToWork();
+    })
+    .then((value) => {
+        console.log(value);
+        return coffe();
+    })
+    .then((value) => {
+        console.log(value);
+        return talkToFriend();
+    })
+    .then((value) => {
+        console.log(value);
+        return chat();
+    })
+    .then((value) => {
+        console.log(value);
+        return goToGym();
+    })
+    .then((value) => {
+        console.log(value);
+        return readTheBook();
+    })
+    .then((value) =>{
+        console.log(value);
+        return bujet();
+    })
+    .then((value) =>{
+        console.log(value);
+        return byDress();
+    })
+    .then((value) =>{
+        console.log(value);
+        return lanch();
+    })
+    .catch(reason =>{
+        console.log('*** ERROR ****')
+        console.log(reason)
+        console.log('**** ERROR ****')
+    })
 
-
-//4
-function manicure(money, cb){
-    setTimeout(()=> {
-        console.log('Зробити манікюр')
-        if(money < 450){
-            console.log('маленький бюджет', null)
-            cb('можемо запропонувати гігієнічний')
-        } else {
-            console.log('комбінований')
-            cb(null, 'ваш майстер')
-        }
-    }, 1400);
-}
-function nailMaster(){
-    setTimeout(() =>{
-        console.log('Аліна')
-    }, 150)
-}
-manicure(475, (err, manicure) => {
-    if(err){
-        console.error(err)
-    } else {
-        nailMaster()
-        console.log(manicure)
-    }
-})
-
-//5
-
-function goToWork (money, cb){
-    setTimeout(()=>{
-        console.log('Добратись на роботу')
-        if(money < 75){
-            console.log('на автобусі', null)
-            cb('купуй квиток')
-        } else {
-            console.log('на таксі')
-            cb(null, 'сьогодні лухарі')
-        }
-    }, 3000);
-}
-function onTaxy(){
-    setTimeout(() => {
-        console.log('їдь на таксі')
-    }, 200)
-}
-goToWork(100, (err, goToWork)=>{
-    if (err){
-        console.error(err)
-    } else {
-        onTaxy();
-        console.log(goToWork)
-    }
-})
-
-
-// function coffe(money, cb){
-//     setTimeout(()=>{
-//         console.log('купити каву')
-//         if(money >45){
-//             console.log('айс лате')
-//         } else {
-//             console.log('американо')
-//         }
-//     }, 700);
-// }
-// coffe(24)
-
-
-//     setTimeout(()=>{
-//         console.log('Поговорити з подругою')
-//     }, 1500);
-
-//     setTimeout(()=>{
-//         console.log('Зварити каву')
-//     }, 100);
-
-//     setTimeout(()=>{
-//         console.log('Посперечатись')
-//     }, 450);
-
-//     setTimeout(()=>{
-//         console.log('Сходити на треню')
-//     }, 2200);
-
-//     setTimeout(()=>{
-//         console.log('Читати книгу')
-//     }, 860);
-
-//     setTimeout(()=>{
-//         console.log('Розпланувати бюджет')
-//     }, 1150);
-
-//     setTimeout(()=>{
-//         console.log('Обрати плаття')
-//     }, 890);
-
-//     setTimeout(()=>{
-//         console.log('Пообідати')
-//     }, 1250);
